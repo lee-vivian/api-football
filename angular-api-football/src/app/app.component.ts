e2e222bba5837628218a0cd44bb4e39705093750
Vivian Lee <vivianallisonlee@gmail.com>
2024-02-24 17:42:41 -0500

    Add angular material and use proxy to enable angular app access to python server

--- angular-api-football/src/app/app.component.ts
+++ angular-api-football/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FootballService } from './services/football.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-api-football';
+
+  constructor(
+    private footballService: FootballService,
+  ){}
+
+  ngOnInit() {
+    this.footballService.getStatus().subscribe({
+      next: (result) => {
+        console.log(result);
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    })
+  }
+
 }
--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS, cross_origin
 import http.client
 import gzip
 import json
@@ -6,6 +7,14 @@ import json
 from env import API_HOST, API_KEY
 
 app = Flask(__name__)
+
+allowed_origins = [
+    "http://localhost:4200",
+]
+
+cors = CORS(app, resources={r"*": {"origins": allowed_origins}})
+
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 def get_headers():
     headers = {
@@ -24,7 +33,7 @@ def get_request(endpoint):
     response_data = response.read()
     decompressed_data = gzip.decompress(response_data)
     decoded_decompressed_data = decompressed_data.decode('utf-8')
-    json_response = json.loads(decoded_decompressed_data)
+    json_response = json.loads(decoded_decompressed_data)['response']
     return json_response
 
 @app.route('/status', methods=['GET'])
